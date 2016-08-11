7e296ecaa447158b7d1412049cbc2a87093b80c6
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2016-08-11 16:23:36 -0400

    instruct a plane to land

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,8 +1,2 @@
 class Plane
-
-  attr_accessor :flying
-
-  def initialize
-    @flying = true
-  end
 end
--- lib/weather.rb
+++
@@ -1,9 +0,0 @@
-module Weather
-
-  attr_reader :weather_warning
-
-  def weather_warning
-    rand(100) > 80 ? :Stormy : :Fine
-  end
-
-end
