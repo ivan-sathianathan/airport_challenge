49d25177db1889e0149305d2e436d3160799f001
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2016-08-11 16:30:25 -0400

    instructs a plane to take off

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,4 +1,5 @@
 describe 'User Stories' do
+
   # As an air traffic controller
   # So planes can land safely at my airport
   # I would like to instruct a plane to land
@@ -6,6 +7,14 @@ describe 'User Stories' do
     airport = Airport.new
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
+  end
 
+  # As an air traffic controller
+  # So planes can take off safely from my airport
+  # I would like to instruct a plane to take off
+  it 'so planes take off from airport, instruct a plane to take off' do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.take_off(plane) }.not_to raise_error
   end
 end
